82c41cb43c918f6d2a349957d9dabd5f1fe9b49b
vardo159 <bramf0@protonmail.com>
2021-11-22 16:00:37 -0500

    initial components added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
-import './App.css';
+import { Container } from 'react-bootstrap';
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
+//import './App.css';
+import LandingPage from './components/LandingPage';
+import LogIn from './components/LogIn';
+import SignUp from './components/SignUp';
 
 function App() {
   return (
     <div className="App">
-        initial
+        <Container>
+          <Router basename="">
+            <Routes>
+              <Route path="/" element={<LandingPage/>}/>
+              <Route path="/signup" element={<SignUp />} />
+              <Route path="/login" element={<LogIn />} />
+            </Routes>
+          </Router>
+        </Container>
     </div>
   );
 }
---
+++ src/components/LandingPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function LandingPage() {
+    return (
+        <div>
+            Principal page
+        </div>
+    )
+}
