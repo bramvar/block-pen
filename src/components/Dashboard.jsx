6984b8d4df4e7fa6d50c7b3e0a0a0d934bbaf666
vardo159 <bramf0@protonmail.com>
2021-12-02 22:56:10 -0500

    update. Do not touch any component realted to Dashboard

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,15 +1,24 @@
+import { MenuItem } from '@mui/material' 
 import React from 'react'
-import { Container, Form, FormControl, Navbar,Button } from 'react-bootstrap'
+import { Container, Form, FormControl, Navbar,Button, Nav } from 'react-bootstrap'
 import { Link, Outlet } from 'react-router-dom'
 
 export default function Dashboard() {
     return (
         <div>
-            <Navbar expand="lg">
+            <Navbar expand="lg" variant="dark" bg="dark">
                 <Container>
                     <Navbar.Brand href="/">Block pen</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <MenuItem component={Link} to={'/myrepo'}>Team 1</MenuItem>
+                    <Link to="/practice">practice</Link>
+                    <Link to="/stats">stats</Link>
                     
-                    <Navbar.Collapse className="justify-content-end">
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                        <Nav className="me-auto">
+                        <Nav.Link href="/myrepo">Repo</Nav.Link>
+                        <Nav.Link href="/practice">Practice</Nav.Link>
+                        </Nav>
                         <Form className="d-flex">
                             <FormControl
                                 type="search"
@@ -25,9 +34,7 @@ export default function Dashboard() {
                     </Navbar.Collapse>
                 </Container>
             </Navbar>              
-            <Link to="/myrepo">repo</Link> 
-            <Link to="/practice">practice</Link>
-            <Link to="/stats">stats</Link>
+            
             <Outlet />
         </div>
     )
