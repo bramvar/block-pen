4ccd6b7615701071218200ceaa57687df2ef64af
vardo159 <bramf0@protonmail.com>
2021-12-03 22:04:27 -0500

    dashboard update

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -6,9 +6,9 @@ import { Link, Outlet } from 'react-router-dom'
 export default function Dashboard() {
     return (
         <div>
-            <Navbar expand="lg" variant="dark" bg="dark">
+            <Navbar expand="lg" variant="dark" bg="dark" className="py-1">
                 <Container className="mx-2">
-                    <Navbar.Brand href="/">
+                    <Navbar.Brand href="/" className="py-0">
                         <img
                             alt=""
                             src="/logo.svg"
@@ -21,8 +21,6 @@ export default function Dashboard() {
                     </Navbar.Brand>
                     <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                     <MenuItem component={Link} to={'/myrepo'}>Team 1</MenuItem>
-                    <Link to="/practice">practice</Link>
-                    <Link to="/stats">stats</Link>
                     
                     <Navbar.Collapse id="responsive-navbar-nav">
                         <Nav className="me-auto">
@@ -36,7 +34,7 @@ export default function Dashboard() {
                                 className="my-2"
                                 aria-label="Search"
                             />
-                            <Button className="h-50 mx-2 py-2 " variant="outline-success">Search</Button>
+                            <Button className="h-50 mx-2 my-2 py-2 " variant="outline-success">Search</Button>
                         </Form>
                         <Navbar.Text>
                             Signed in as: <a href="#r">M</a>
